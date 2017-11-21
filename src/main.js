a23b6a25b8f2b7d06d0abf52c5cc0bfc60ac4ebb
jim <812329595@qq.com>
2017-11-21 23:47:34 +0800

    导航

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
 
+// 解决移动端300毫秒演示问题
+
+import fastclick from 'fastclick'
+
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
